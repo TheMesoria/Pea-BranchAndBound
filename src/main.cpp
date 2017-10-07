ceff0fb114953a058390f14c60fa0e06a417d200
TheMesoria <freedomandjustit@gmail.com>
2017-10-07 19:08:57 +0200

    Init

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+//	PEA-Ormaniec-Branch&Bound
+//	Created by Ormaniec
+//	At 10/7/2017
+//	All rights reserved
+//
+
+#include <string>
+#include <iostream>
+
+int main(int argc, char *args[])
+{
+	return EXIT_SUCCESS;
+}
